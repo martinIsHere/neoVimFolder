a79d825337a807c02faa7d742b7fbdfd8d3e8add
martinIsHere <martin.havardsen@gmail.com>
2025-11-19 18:35:47 +0100

    Set up new t bind, and unmapped from flash

--- lua/vim-keybinds.lua
+++ lua/vim-keybinds.lua
@@ -1,6 +1,36 @@
 -- vim.api.nvim_set_keymap("i", "jk", "<ESCAPE>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<leader>w", "<cmd>w<CR>", { noremap = true, silent = true, desc = "[W]rite buffer" })
 
+-- split keybind
+vim.api.nvim_set_keymap(
+	"n",
+	"<leader>s",
+	"<cmd>vsp<CR>",
+	{ noremap = true, silent = true, desc = "[S]plit vertically" }
+)
+vim.api.nvim_set_keymap(
+	"n",
+	"<leader>S",
+	"<cmd>sp<CR>",
+	{ noremap = true, silent = true, desc = "[S]plit horizontally" }
+)
+
+-- mapping "t"
+pcall(vim.api.nvim_del_keymap, "n", "t")
+pcall(vim.api.nvim_del_keymap, "n", "T")
+vim.api.nvim_set_keymap(
+	"n",
+	"t",
+	"}",
+	{ noremap = true, silent = true, desc = "[T]ake step forward to next paragraph" }
+)
+vim.api.nvim_set_keymap(
+	"n",
+	"T",
+	"{",
+	{ noremap = true, silent = true, desc = "[T]ake step backward to next paragraph" }
+)
+
 -- removes the ^M line endings when copying from windows to wsl
 vim.api.nvim_set_keymap(
 	"n",
@@ -14,13 +44,11 @@ vim.api.nvim_set_keymap(
 	"n",
 	"<leader>ybn",
 	":let @+ = expand(' % ')<CR>",
-	{ noremap = true, silent = true, desc = "[C]opy buffer [N]ame to clipboard" }
+	{ noremap = true, silent = true, desc = "[Y]ank [B]uffer [N]ame to clipboard" }
 )
 vim.api.nvim_set_keymap(
 	"n",
 	"<leader>yfp",
 	":let @+ = expand('%:p')<CR>",
-	{ noremap = true, silent = true, desc = "[C]opy file [P]ath to clipboard" }
+	{ noremap = true, silent = true, desc = "[Y]ank [F]ile [P]ath to clipboard" }
 )
-
-vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
